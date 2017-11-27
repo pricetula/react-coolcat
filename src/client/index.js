094db62df28463ecf6036bee40f7d83d06ab67d2
Price Tula <pricetula@gmail.com>
2017-11-27 20:41:23 +0300

    using hydrate

--- src/client/index.js
+++ src/client/index.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+/* eslint-disable no-undef */
+import React from 'react';
+import { hydrate } from 'react-dom';
+import App from '../common/App';
 
-import './style.css';
-
-export default class Hello extends Component {
-  render() {
-    return (
-      <div className="test">
-        Hello  world
-      </div>
-    );
-  }
-}
-
-render(<Hello />, document.getElementById('react-root'));
+hydrate(
+  <App />,
+  document.getElementById(
+    'react-root',
+  ),
+);
