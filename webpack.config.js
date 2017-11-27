bde97fee8dad607be55b8958e195e4898ba2a1d7
Price Tula <pricetula@gmail.com>
2017-11-27 20:41:52 +0300

    server & client configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,47 @@
 // running dev will cause webpack to look for this config file.
 // dev is running with --hot for hot module replacement
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+/* eslint-disable comma-dangle */
+const path = require(
+  'path'
+);
+const HtmlWebpackPlugin = require(
+  'html-webpack-plugin'
+);
+const ExtractTextPlugin = require(
+  'extract-text-webpack-plugin'
+);
 
 // Constant with our paths
 const paths = {
-  dist: path.resolve(__dirname, 'dist'),
-  client: path.resolve(__dirname, 'src/client'),
+  dist: path.resolve(
+    __dirname,
+    'dist'
+  ),
+  client: path.resolve(
+    __dirname,
+    'src/client'
+  ),
+  server: path.resolve(
+    __dirname,
+    'src/server'
+  )
 };
 
 // Webpack configuration
-module.exports = {
-  entry: path.join(paths.client, 'index.js'),
+const clientConfig = {
+  entry: path.join(
+    paths.client,
+    'index.js'
+  ),
 
   // output of build and bundle.js as build output
   output: {
-    path: paths.dist,
+    path: `${paths.dist}/public`,
     filename: 'bundle.js'
+  },
+
+  node: {
+    fs: 'empty'
   },
 
   // loaders for js or jsx files to be parsed by babel
@@ -27,29 +51,31 @@ module.exports = {
         test: /\.(js|jsx)$/, // find both js or jsx files
         exclude: /node_modules/,
         use: [
-          'babel-loader', // using babel loader
-        ],
+          'babel-loader' // using babel loader
+        ]
       },
 
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          use: 'css-loader',
-        }),
+        loader: ExtractTextPlugin.extract(
+          {
+            use: 'css-loader'
+          }
+        )
       },
 
       {
         test: /\.(png|jpg|gif)$/,
         use: [
-          'file-loader',
-        ],
-      },
-    ],
+          'file-loader'
+        ]
+      }
+    ]
   },
 
   // resolve js or jsx files to be loaded by babel
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx']
   },
 
   // this will generate a file dist/index.html
@@ -58,9 +84,57 @@ module.exports = {
       {
         title: 'Cool Cat Dev',
         // filename specify new file name
-        template: path.join(paths.dist, 'index.html'), // merge our index.html with the plugin one
+        template: path.join(
+          paths.dist,
+          'index.html'
+        ) // merge our index.html with the plugin one
       }
     ),
-    new ExtractTextPlugin('style.css'), // all styles will be bundled to dist by plugin
+    new ExtractTextPlugin(
+      'style.css'
+    ) // all styles will be bundled to dist by plugin
   ]
 };
+
+const serverConfig = {
+  entry: path.join(
+    paths.server,
+    'index.js'
+  ),
+
+  // output of build and bundle.js as build output
+  output: {
+    path: paths.dist,
+    filename: 'server.js',
+    libraryTarget: 'commonjs2'
+  },
+
+  target: 'node',
+
+  node: {
+    fs: 'empty'
+  },
+
+  // loaders for js or jsx files to be parsed by babel
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // find both js or jsx files
+        exclude: /node_modules/,
+        use: [
+          'babel-loader' // using babel loader
+        ]
+      }
+    ]
+  },
+
+  // resolve js or jsx files to be loaded by babel
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
+
+module.exports = [
+  serverConfig,
+  clientConfig
+];
