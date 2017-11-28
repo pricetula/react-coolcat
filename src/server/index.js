c94cd6187066521a24c59f206b2f93a4f0793351
Price Tula <pricetula@gmail.com>
2017-11-28 08:53:49 +0300

    server index

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable function-paren-newline */
 import winston from 'winston';
 // import path from 'path';
 // import toPlainObject from 'lodash/toPlainObject';
@@ -5,9 +6,10 @@ import winston from 'winston';
 import axios from 'axios';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import StaticRouter from 'react-router-dom/StaticRouter';
 import express from 'express';
 import cors from 'cors';
-import App from '../common/App';
+import appComponent from '../common/component/app';
 
 function renderHTML(
   component,
@@ -36,7 +38,7 @@ const app = express(
 
 app.use(
   express.static(
-    'public',
+    'dist',
   ),
 );
 
@@ -70,9 +72,12 @@ app.get(
             renderHTML(
               renderToString(
                 // passing state through props not sure here
-                <App
-                  appState={response.data}
-                />,
+                <StaticRouter location={req.url} context={{}}>
+                  {
+                    appComponent(
+                    )
+                  }
+                </StaticRouter>,
               ),
               // Pass object data to preloaded state so that it will be available to component
               { appState: response.data },
