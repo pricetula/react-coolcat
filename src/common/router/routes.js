eb4eace34414051965fdfbb94ebe5930fb0731e3
Price Tula <pricetula@gmail.com>
2017-11-30 21:06:35 +0300

    reverted to old way not splitting

--- src/common/router/routes.js
+++ src/common/router/routes.js
@@ -30,3 +30,30 @@ const routes = [
 ];
 
 export default routes;
+
+/*
+import App from '../component/App';
+import Home from '../component/Home';
+import Todo from '../component/Todo';
+import Simple from '../component/Simple';
+import Counter from '../component/Counter';
+
+//splitting
+import loadable from 'loadable-components';
+const Home = loadable(
+  (
+  ) => import('../component/Home')
+);
+const Todo = loadable(
+  (
+  ) => import('../component/Todo')
+);
+const Simple = loadable(
+  (
+  ) => import('../component/Simple')
+);
+const Counter = loadable(
+  (
+  ) => import('../component/Counter')
+);
+*/
