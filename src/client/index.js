c9db4ad9509c3c88e688174792a20f251f6c7820
Price Tula <pricetula@gmail.com>
2017-12-08 23:11:05 +0300

    added theme on client

--- src/client/index.js
+++ src/client/index.js
@@ -3,16 +3,20 @@ import React from 'react';
 import { hydrate } from 'react-dom';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import { MuiThemeProvider } from 'material-ui/styles';
 import app from '../common/component/app';
 import store from '../common/redux/sagaPort';
+import theme from '../common/mui/theme';
 
 hydrate(
   <Provider store={store}>
     <HashRouter>
-      {
-        app(
-        )
-      }
+      <MuiThemeProvider theme={theme}>
+        {
+          app(
+          )
+        }
+      </MuiThemeProvider>
     </HashRouter>
   </Provider>,
   document.getElementById(
