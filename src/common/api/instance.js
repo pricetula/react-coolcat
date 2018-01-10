2bf317e20ce44fafb066b52ea76c8c7dfa0004b6
price tula <pricetulak@gmail.com>
2018-01-10 15:57:34 +0300

    fix

--- src/common/api/instance.js
+++ src/common/api/instance.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
+let baseURL = 'http://localhost:3030/';
+
+if (process.env.NODE_ENV === 'production') {
+  baseURL = 'https://express-coolcat.herokuapp.com';
+}
+
 const instance = axios.create(
   {
-    baseURL: 'http://localhost:3030/',
+    baseURL,
     timeout: 2000,
-  }
+  },
 );
 
 export default instance;
